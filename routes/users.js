62471a08d1e7f432f7d08c729087a114e0e82a60
A K M Rezaul Karim <tito@cefalo.com>
2020-05-08 17:40:08 +0600

    BE #2 user login using JWT

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,19 @@
 var express = require('express');
 var User =require('../models/user');
+var passport = require('passport');
+var authenticate = require('../authenticate');
 
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/',authenticate.verifyUser, function(req, res, next) {
+  User.find({})
+    .then((users)=>{
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'application/json');
+      res.json(users);
+    }, (err) => next(err))
+    .catch((err) => next(err));
 });
 
 router.post('/signup', (req, res, next) => {
@@ -37,6 +45,14 @@ router.post('/signup', (req, res, next) => {
       });
     }
   });
+});
+
+router.post('/login', passport.authenticate('local'), (req, res) => {
+
+  var token = authenticate.getToken({_id: req.user._id});
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'application/json');
+  res.json({success: true, token: token, status: 'You are successfully logged in!'});
 });
 
 module.exports = router;
