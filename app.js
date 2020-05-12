1e3e4c4f3d29549f80f17c2da70473a7d35bf5c4
Ahmed Maruf <maruf@cefalo.com>
2020-05-12 15:59:22 +0600

    Connect to mongodb using mongoose client

--- app.js
+++ app.js
@@ -3,10 +3,9 @@ const logger = require('morgan');
 
 const app = express();
 
+
 app.get('/', (req, res) => {
     res.send('Hello CMS!')
 });
 
-app.listen(3000, () => {
-    console.log('Server started and listening on PORT: 3000');
-});
+module.exports = app;
--- start.js
+++ start.js
@@ -1,1 +1,29 @@
-const mongoose = require();
+const mongoose = require('mongoose');
+
+const [major, minor] = process.versions.node.split('.').map(parseFloat);
+
+if (major < 12 || (major === 12 && minor <= 10)) {
+	console.log('🛑 🌮 🐶 💪 💩\nHey You! \n\t ya you! \n\t\tBuster! \n\tYou\'re on an older version of node that doesn\'t support the latest and greatest things we are learning (Async + Await)! Please go to nodejs.org and download version 7.6 or greater. 👌\n ');
+	process.exit();
+}
+
+// import environmental variables from our variables.env file
+require('dotenv').config({
+	path: 'variables.env'
+});
+
+// connect to our database and handle bad connection
+mongoose.connect(process.env.DATABASE, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true
+});
+mongoose.Promise = global.Promise; //tell mongoose to use es6 promise
+mongoose.connection.on('error', (error) => {
+	console.error(`🙅 🚫 🙅 🚫 🙅 🚫 🙅 🚫 → ${err.message}`);
+});
+
+const app = require('./app');
+app.set('port', process.env.PORT || 7777);
+const server = app.listen(app.get('port'), () =>{
+	console.log(`Express running → PORT ${server.address().port}`);
+});
