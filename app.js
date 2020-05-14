49d2e88a43333510cbebf15d41e6aa0bf1ef30af
A K M Rezaul Karim <tito@cefalo.com>
2020-05-14 19:14:10 +0600

    BE #13 add env settings, connect to db, cleanup static files

--- app.js
+++ app.js
@@ -1,19 +1,33 @@
+var debug = require('debug')('cms-backend-api:*');
 var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mongoose=require('mongoose');
 
+//custom modules
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+//connect to database
+var connect = mongoose.connect(process.env.DB_URL,{
+    autoIndex: false,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+connect.then((db) => {
+    debug('Connected to database ' + process.env.DB_URL);
+}, (err) => { console.log(err); });
+
+
+
 var app = express();
 
+//middleware
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
+//routes
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
