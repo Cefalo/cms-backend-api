b8eaf373dc378056cb88c1fc5a2ecbdf517de7da
Satyajit Dey <satyajit.dey@varnish-software.com>
2020-05-14 17:59:53 +0600

    Project Init using express-generator

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
-const express = require('express');
-const logger = require('morgan');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
-const app = express();
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 
-app.get('/', (req, res) => {
-    res.send('Hello CMS!')
-});
+var app = express();
 
-app.listen(3000, () => {
-    console.log('Server started and listening on PORT: 3000');
-});
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+module.exports = app;
