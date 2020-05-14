67191d173309327e4dffb3dfb0d618e760ddde18
A K M Rezaul Karim <tito@cefalo.com>
2020-05-14 20:12:14 +0600

    BE #1 User registration added

---
+++ models/user.js
@@ -0,0 +1,31 @@
+const mongoose=require('mongoose');
+const Schema=mongoose.Schema;
+
+var passportLocalMongoose = require('passport-local-mongoose');
+
+var User = new Schema({
+    fullname: {
+        type: String,
+        default: ''
+    },
+    email:   {
+        type: String,
+        default: ''
+    }
+});
+
+User.plugin(passportLocalMongoose,{
+    usernameLowerCase:true,
+    passwordValidator : (password,cb)=>{
+        if (!password.trim()) {
+            return cb('Password can not be empty!');
+        }
+        else if(password.trim().length<8){
+            return cb('Password must be at least 8 chars long');
+        }
+        // return an empty cb() on success
+        return cb()
+      }
+});
+
+module.exports = mongoose.model('User', User);
