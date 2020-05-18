6bb08ddf38174e788b9bab5fda99dbc965b38b32
Ahmed Maruf <maruf@cefalo.com>
2020-05-18 15:13:12 +0600

    Implement pre hook features to generate unique url

--- models/article.js
+++ models/article.js
@@ -21,5 +21,28 @@ var articleSchema = new Schema({
     timestamps: true
 });
 
+articleSchema.pre('save', async function (next) {
+    if (!this.isModified('title')){
+        return next();
+    }
+    
+    this.url = slugify(this.title, {
+        lower: true,      // convert to lower case
+        strict: true,     // strip special characters except replacement
+    });
+    
+    //Todo make slug more resiliant to slugs are unique
+    // find other stores that have a slug of title, title-1, title-2
+    
+    const urlRegEx = new RegExp(`^(${this.url})((-[0-9]*$)?)$`, 'i');
+    const articleWithUrl = await this.constructor.find({url: urlRegEx}).exec();
+    
+    if (articleWithUrl.length) {
+        this.url = `${this.url}-${articleWithUrl.length + 1}`;
+    }
+    
+    next();
+});
+
 var Article=mongoose.model('Article',articleSchema);
 module.exports=Article;
