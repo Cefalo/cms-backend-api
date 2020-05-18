91299925b80aad1d35cd6ecb5dfadcea8c0ae163
A K M Rezaul Karim <tito@cefalo.com>
2020-05-18 16:40:30 +0600

    BE #4 cleanup article route

--- models/article.js
+++ models/article.js
@@ -1,25 +1,48 @@
-var mongoose=require('mongoose');
-var Schema=mongoose.Schema;
-
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var slugify = require('slugify');
 
 var articleSchema = new Schema({
-    title:{
-        type:String,
-        require:true
-    },
-    url:{
-        type:String,
-        require:true
-    },
-    body:[String],
-    author: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
-        required: true,
-    }
+	title: {
+		type: String,
+		require: true
+	},
+	url: {
+		type: String,
+		require: true
+	},
+	body: [String],
+	author: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: 'User',
+		required: true,
+	}
 }, {
-    timestamps: true
+	timestamps: true
 });
 
-var Article=mongoose.model('Article',articleSchema);
-module.exports=Article;
+articleSchema.pre('save', async function (next) {
+	if (!this.isModified('title')) {
+		return next();
+	}
+
+	this.url = slugify(this.title, {
+		lower: true,
+		strict: true,
+	});
+
+	//Todo make slug more resiliant to slugs are unique
+	// find other stores that have a slug of title, title-1, title-2
+
+	const urlRegEx = new RegExp(`^(${this.url})((-[0-9]*$)?)$`, 'i');
+	const storesWithUrl = await this.constructor.find({ url: urlRegEx }).exec();
+
+	if (storesWithUrl.length) {
+		this.url = `${this.url}-${storesWithUrl.length}`;
+	}
+
+	next();
+});
+
+var Article = mongoose.model('Article', articleSchema);
+module.exports = Article;
--- routes/article.js
+++ routes/article.js
@@ -2,7 +2,6 @@ var debug = require('debug')
 var express = require('express');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
-var slugify = require('slugify')
 var auth = require('../middlewares/auth.validation.middleware');
 
 var Article = require('../models/article');
@@ -10,178 +9,132 @@ var Article = require('../models/article');
 var router = express.Router();
 router.use(bodyParser.json());
 
-var createUniqueUrl = (title, callBack, counter = 0) => {
-    var url = slugify(title, {
-        lower: true,      // convert to lower case
-        strict: true,     // strip special characters except replacement
-    });
-
-    if (counter > 0) {
-        url += '-' + counter;
-    }
-
-    Article.findOne({ url: url })
-        .then((art) => {
-            if (null === art) {
-                callBack(url);
-            }
-            else {
-                createUniqueUrl(title, callBack, counter + 1);
-            }
-        })
-}
-
 
 router.route('/')
-    .get((req, res, next) => {
-        Article.find({})
-            .sort({ 'updatedAt': -1 })
-            .limit(5)
-            .populate('author')
-            .exec(function (err, articles) {
-                if (err) {
-                    res.statusCode = 500;
-                    res.setHeader('Content-Type', 'application/json');
-                    res.json(err);
-                }
-                else {
-                    res.statusCode = 200;
-                    res.setHeader('Content-Type', 'application/json');
-                    res.json(articles);
-                }
-            });
-    })
-    .post(auth.verifyJwtToken, (req, res, next) => {
-        createUniqueUrl(req.body.title, (url) => {
-            var article = new Article({ title: req.body.title });
-            article.url = url;
-            article.body = req.body.body;
-            article.author = req.jwt.userId;
+	.get((req, res, next) => {
+		Article.find({})
+			.sort({ 'updatedAt': -1 })
+			.limit(5)
+			.populate('author')
+			.exec(function (err, articles) {
+				if (err) {
+					var err = new Error(err);
+					err.status = 500;
+					return next(err);
+				}
+				else {
+					res.statusCode = 200;
+					res.setHeader('Content-Type', 'application/json');
+					res.json(articles);
+				}
+			});
+	})
+	.post(auth.verifyJwtToken, (req, res, next) => {
 
-            article.save((err, art) => {
-                if (err) {
-                    res.statusCode = 500;
-                    res.setHeader('Content-Type', 'application/json');
-                    res.json(err);
-                }
-                else {
-                    res.statusCode = 200;
-                    res.setHeader('Content-Type', 'application/json');
-                    res.json(art);
-                }
-            })
-        });
-    })
-    .put((req, res, next) => {
-        res.statusCode = 403;
-        res.end('PUT operation not supported on /article');
-    })
-    .delete((req, res, next) => {
-        res.statusCode = 403;
-        res.end('DELETE operation not supported on /article');
-    });
+		Article.create({
+			title: req.body.title,
+			body: req.body.body,
+			author: req.jwt.userId
+		})
+		.then((article) => {
+			res.statusCode = 200;
+			res.setHeader('Content-Type', 'application/json');
+			res.json(article);
+		}, (err) => next(err))
+		.catch((err) => next(err));
+
+	})
+	.put((req, res, next) => {
+		var err = new Error('PUT operation not supported on /article');
+		err.status = 403;
+		return next(err);
+	})
+	.delete((req, res, next) => {
+		var err = new Error('DELETE operation not supported on /article');
+		err.status = 403;
+		return next(err);
+	});
 
 
 /* with id */
 router.route('/:articleId')
-    .get((req, res, next) => {
-        Article.findById(req.params.articleId)
-            .populate('author')
-            .then((article) => {
-                res.statusCode = 200;
-                res.setHeader('Content-Type', 'application/json');
-                res.json(article);
-            }, (err) => next(err))
-            .catch((err) => next(err));
-    })
-    .post((req, res, next) => {
-        res.statusCode = 403;
-        res.end('POST operation not supported on /article/' + req.params.articleId);
-    })
-    .put(auth.verifyJwtToken, (req, res, next) => {
+	.get((req, res, next) => {
+		Article.findById(req.params.articleId)
+			.populate('author')
+			.then((article) => {
+				if (null === article) {
+					var err = new Error('Article ' + req.params.articleId + ' not found');
+					err.status = 404;
+					return next(err);
+				}
+				else {
+					res.statusCode = 200;
+					res.setHeader('Content-Type', 'application/json');
+					res.json(article);
+				}
+			}, (err) => next(err))
+			.catch((err) => next(err));
+	})
+	.post((req, res, next) => {
+		var err = new Error('POST operation not supported on /article/' + req.params.articleId);
+		err.status = 403;
+		return next(err);
+	})
+	.put(auth.verifyJwtToken, (req, res, next) => {
 
-        if (mongoose.Types.ObjectId.isValid(req.params.articleId)) {
+		Article.findByIdAndUpdate(req.params.articleId, {
+			$set: req.body
+		}, { new: true })
+			.then((upudatedArt) => {
+				if (null === upudatedArt) {
+					var err = new Error('Article ' + req.params.articleId + ' not found');
+					err.status = 404;
+					return next(err);
+				}
+				else {
+					res.statusCode = 200;
+					res.setHeader('Content-Type', 'application/json');
+					res.json(upudatedArt);
+				}
 
-            Article.findById(req.params.articleId)
-                //.populate('author')
-                .then((article) => {
+			}, (err) => next(err))
+			.catch((err) => next(err));
+	})
+	.delete(auth.verifyJwtToken, (req, res, next) => {
+		if (mongoose.Types.ObjectId.isValid(req.params.articleId)) {
+			Article.findById(req.params.articleId)
+				.then((art) => {
+					if (null === art) {
+						var err = new Error('Article ' + req.params.articleId + ' not found');
+						err.status = 404;
+						return next(err);
+					}
+					else {
+						//only creator can delete his article
+						if (art.author == req.jwt.userId) {
+							Article.findByIdAndRemove(req.params.articleId)
+								.then(rem => {
+									res.statusCode = 200;
+									res.setHeader('Content-Type', 'application/json');
+									res.json(art);
+								});
+						}
+						else {
+							var err = new Error('You do not have permission to delete this article!');
+							err.status = 403;
+							return next(err);
+						}
 
-                    if (null === article) {
-                        var err = new Error('Article ' + req.params.articleId + ' not found');
-                        err.status = 404;
-                        return next(err);
-                    }
-                    else {
-                        //update url as title is changed
-                        if (article.title !== req.body.title) {
-                            createUniqueUrl(req.body.title, (url) => {
-                                Article.findByIdAndUpdate(req.params.articleId, {
-                                    $set: { "title": req.body.title, "url": url, "body": req.body.body }
-                                }, { new: true })
-                                    .then((upudatedArt) => {
-                                        res.statusCode = 200;
-                                        res.setHeader('Content-Type', 'application/json');
-                                        res.json(upudatedArt);
-                                    }, (err) => next(err))
-                                    .catch((err) => next(err));
-                            });
-                        }
-                        else {
-                            Article.findByIdAndUpdate(req.params.articleId, {
-                                $set: req.body
-                            }, { new: true })
-                                .then((upudatedArt) => {
-                                    res.statusCode = 200;
-                                    res.setHeader('Content-Type', 'application/json');
-                                    res.json(upudatedArt);
-                                }, (err) => next(err))
-                                .catch((err) => next(err));
-                        }
-                    }
-                }, (err) => next(err))
-                .catch((err) => next(err));
-        }
-        else {
-            var err = new Error('Article ' + req.params.articleId + ' not found');
-            err.status = 404;
-            return next(err);
-        }
-    })
-    .delete(auth.verifyJwtToken, (req, res, next) => {
-        if (mongoose.Types.ObjectId.isValid(req.params.articleId)) {
-            Article.findById(req.params.articleId)
-                .then((art) => {
-                    if (null === art) {
-                        var err = new Error('Article ' + req.params.articleId + ' not found');
-                        err.status = 404;
-                        return next(err);
-                    }
-                    else {
-                        //only creator can delete his article
-                        if (art.author.id == req.jwt.userId) {
-                            Article.findByIdAndRemove(req.params.articleId)
-                                .then(rem => {
-                                    res.statusCode = 200;
-                                    res.setHeader('Content-Type', 'application/json');
-                                    res.json(art);
-                                });
-                        }
-                        else {
-                            var err = new Error('You do not have permission to delete this article!');
-                            err.status = 403;
-                            return next(err);
-                        }
+					}
 
-                    }
-
-                }, (err) => next(err))
-                .catch((err) => next(err));
-        }
-        else {
-            var err = new Error('Article ' + req.params.articleId + ' not found');
-            err.status = 404;
-            return next(err);
-        }
-    });
+				}, (err) => next(err))
+				.catch((err) => next(err));
+		}
+		else {
+			var err = new Error('Article ' + req.params.articleId + ' not found');
+			err.status = 404;
+			return next(err);
+		}
+	});
 
 module.exports = router;
