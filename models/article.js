8a1ef76f99200c4beb36d05808766fd5e06dff7a
A K M Rezaul Karim <tito@cefalo.com>
2020-05-19 15:48:04 +0600

    BE #4 complex article body elements

--- models/article.js
+++ models/article.js
@@ -2,16 +2,82 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var slugify = require('slugify');
 
+const optsVirtual = { toJSON: { virtuals: true } };
+
+var propSchema = new Schema({
+	name: {
+		type: String,
+		require: true,
+		trim: true
+	},
+	value: {
+		type: String,
+		require: true,
+		trim: true
+	}
+});
+
+var bodySchema = new Schema({
+	tag: {
+		type: String,
+		trim: true,
+		lowercase: true
+	},
+	innerHTML: {
+		type: String
+	},
+	props: [propSchema]
+},optsVirtual);
+
+//obj to html generator
+function getHtml(obj){
+	var str;
+	if(obj.tag){
+		str='<'+obj.tag;
+	};
+	//add props
+	str+=obj.props.map(p=>' '+p.name+'="'+p.value+'"').join('');
+
+	if(!obj.innerHTML && !obj.children.length){
+		str+='/>';
+	}
+	else{
+		str+='>';
+		if(obj.children.length){
+			obj.children.forEach(ele=>{
+				str+=getHtml(ele);
+			});
+		}
+		else{
+			str+=obj.innerHTML;
+		}
+		str+='</'+obj.tag+'>';
+	}
+	return str;
+}
+
+bodySchema.virtual('html').get(function() {
+	return getHtml(this);
+});
+
+//recursive elements
+bodySchema.add({
+	children:[bodySchema]
+});
+
+
+// main schema
 var articleSchema = new Schema({
 	title: {
 		type: String,
-		require: true
+		require: true,
+		trim: true
 	},
 	url: {
 		type: String,
 		require: true
 	},
-	body: [String],
+	body: [bodySchema],
 	author: {
 		type: mongoose.Schema.Types.ObjectId,
 		ref: 'User',
@@ -45,4 +111,5 @@ articleSchema.pre('save', async function (next) {
 });
 
 var Article = mongoose.model('Article', articleSchema);
+
 module.exports = Article;
